6c0e9d3415688c935a17e63069e7cb0682d07728
satyajit <satyajit.1418@gmail.com>
2026-09-10 01:18:51 +0530

    Update trivyImageScan.groovy

--- vars/trivyImageScan.groovy
+++ vars/trivyImageScan.groovy
@@ -7,11 +7,15 @@ def call(Map config = [:]) {
     def image = config.image
 
     sh """
+        mkdir -p "\$WORKSPACE/.trivy-cache"
+
         echo "=========================================="
         echo "Scanning Docker Image"
         echo "Image: ${image}"
         echo "=========================================="
 
-        trivy image ${image} > trivyimage.txt
+        trivy image \
+            --cache-dir "\$WORKSPACE/.trivy-cache" \
+            "${image}"
     """
 }
