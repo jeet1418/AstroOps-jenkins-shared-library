63bb197d7e840aa7f9af05895953473b56329e47
satyajit <satyajit.1418@gmail.com>
2026-09-10 01:36:19 +0530

    Update commitAndPushHelm.groovy

--- vars/commitAndPushHelm.groovy
+++ vars/commitAndPushHelm.groovy
@@ -3,9 +3,11 @@ def call(Map config = [:]) {
     if (!config.credentialsId) {
         error "Missing required parameter: credentialsId"
     }
+
     if (!config.gitUrl) {
         error "Missing required parameter: gitUrl"
     }
+
     if (!config.commitMessage) {
         error "Missing required parameter: commitMessage"
     }
@@ -14,16 +16,14 @@ def call(Map config = [:]) {
     def gitUrl        = config.gitUrl
     def commitMessage = config.commitMessage
 
-    sh '''
+    sh """
         git config user.email "jenkins@example.com"
         git config user.name "Jenkins"
 
         git add .
 
-        git commit \
-            -m "${COMMIT_MESSAGE}" \
-            || echo "No changes to commit"
-    '''
+        git diff --cached --quiet || git commit -m "${commitMessage}"
+    """
 
     withCredentials([
         usernamePassword(
@@ -35,7 +35,7 @@ def call(Map config = [:]) {
 
         sh """
             git push \
-                https://\${GIT_USERNAME}:\${GIT_PASSWORD}@${gitUrl} \
+                https://\\\${GIT_USERNAME}:\\\${GIT_PASSWORD}@${gitUrl} \
                 HEAD:main
         """
     }
